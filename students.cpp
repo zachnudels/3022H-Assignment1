55bdaef8f8739ad7841b7f82c5066aa871b6c19f
Zach Nudelman <zachnudels@gmail.com>
2018-03-02 12:06:16 +0200

    Created skeleton structures for students source file and header file

---
+++ students.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std {
+  struct studentrecord{
+    //Name (String), Surname (String), StudentNumber (String), ClassRecord (String).
+    string name;
+    string surname;
+    string studentNumber;
+    string classRecord;
+  }
+
+  vector<StudentRecord> records;
+
+  NDLZAC001::addStudent(string name,string surname,string studentNumber,string classRecord){
+
+  }
+
+  NDLZAC001::readDatabase(){
+
+  }
+
+  NDLZAC001::saveDatabase(string filename){
+
+  }
+
+  NDLZAC001::displayStudentData(string studentNumber){
+
+  }
+
+  NDLZAC001::gradeStudent(string studentNumber){
+
+  }
+
+} /*std*/
---
+++ students.h
@@ -0,0 +1,12 @@
+#ifndef _students_h
+#define _students_h
+
+namespace NDLZAC001{
+
+  void addStudent();
+  readDatabase();
+  void saveDatabase(string filename);
+  void displayStudentData(string studentNumber);
+  void gradeStudent(string studentNumber);
+
+}
